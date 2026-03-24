b66f8f1f709fff0eaba20922585b397b6b7e2770
Kathiravan-S-7 <kathiravan.profo.in@gmail.com>
2026-03-24 09:22:08 +0530

    Create sort_athlete_data_by_column.py

---
+++ Arrays/sort_athlete_data_by_column.py
@@ -0,0 +1,25 @@
+# Sort a 2D list of athlete data based on a specified column index while preserving row order for equal values
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+
+    arr.sort(key=lambda x: x[k])
+
+    for row in arr:
+        print(*row)
